134e1710bfc1cd93661bba83fc85297396aac8e9
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-10-30 16:35:20 +0530

    Update user_register.php
    add names and required attributes

--- src/user/user_register.php
+++ src/user/user_register.php
@@ -13,23 +13,23 @@
 
     <div>
         <form action="" method="post">
-            First Name : <input type="text"> <br>
-            Last Name : <input type="text"> <br>
-            Date of Birth : <input type="date" id ="bday"> <br>
-            Contact Number : <input type="number" min="0"> <br>
-            Home Address : <textarea> </textarea><br>
-            Height : <input type="number" placeholder="Optional (centimeters)" min="0"> <br>
-            Weight : <input type="number" placeholder="Optional (kilograms)" min="0"> <br>
+            First Name : <input type="text" name="firstName" required> <br>
+            Last Name : <input type="text" name="lastName" required> <br>
+            Date of Birth : <input type="date" id ="bday" name="birthday" required> <br>
+            Contact Number : <input type="number" min="0" name="contactNum" required> <br>
+            Home Address : <textarea name="homeAddress" required> </textarea><br>
+            Height : <input type="number" placeholder="Optional (centimeters)" min="0" name="height"> <br>
+            Weight : <input type="number" placeholder="Optional (kilograms)" min="0" name="weight"> <br>
             Gender : 
                 <input type="radio" name="gender" value="m"> Male
                 <input type="radio" name="gender" value="f"> Female <br>
-            Email Address : <input type="email"> <br>
-            Username : <input type="text"> <br>
+            Email Address : <input type="email" name="emailAddress" required> <br>
+            Username : <input type="text" required minlength="6"> Minimum length of 6<br>
             <div>
-                Password : <input type="password"> <button class="togglePassword"> Show Password</button> Password length must be greater than 8 characters. Must include a capital letter, number<br>
+                Password : <input type="password" minlength="8" required> <button class="togglePassword"> Show Password</button> Password length must be atleast 8 characters. Must include a capital letter, number<br>
             </div>
             <div>
-                Confirm Password : <input type="password"> <button class="togglePassword"> Show Password</button> <br>
+                Confirm Password : <input type="password" required> <button class="togglePassword"> Show Password</button> <br>
             </div>
             <div id="medCon">
                 Medical Concerns : (Maximum of 5)<br>
@@ -37,15 +37,15 @@
             </div>
             <div id="emergencyDetails">
                 Emergency Contact Details: <br>
-                Name: <input type="text"> <br>
-                Relationship: <input type="text"> <br>
-                Contact Number: <input type="number" min="0"> <br>
+                Name: <input type="text" name="name" required> <br>
+                Relationship: <input type="text" name="relationship" required> <br>
+                Contact Number: <input type="number" min="0" name="contactNum" required> <br>
                 Want to Add More? (Maximum of 3)
                 <br>
                 <button id="emergencyDetailsbtn">Add More</button>
             </div>
 
-            <button type="submit" id="register"> Register </button>
+            <button type="submit" id="register" onclick="return validateForm(event)"> Register </button>
         </form>
     </div>
 
