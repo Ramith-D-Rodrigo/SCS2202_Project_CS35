785a47b9c35673b7f5897f513ca4456e856770e5
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-10-31 20:52:23 +0530

    Update user.php
    fixed issues, and made changes to debugging echos, private function for medical concerns

--- src/user/user.php
+++ src/user/user.php
@@ -47,10 +47,10 @@ class User{
         $database -> real_escape_string($this -> password))); 
 
         if ($result === TRUE) {
-            echo "New record created successfully\n";
+            echo "New log in details record created successfully<br>";
         }
         else{
-            echo "Error\n";
+            echo "Error<br>";
         }
     }
 
@@ -68,7 +68,7 @@ class User{
         `height`, 
         `weight`) 
         VALUES 
-        (UUID_TO_BIN('%s', 1),'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')",
+        (UUID_TO_BIN('%s', 1),'%s','%s','%s','%s','%s','%s','%s','%s', NULLIF('%s', ''), NULLIF('%s', ''))",
         $database -> real_escape_string($this -> userID),
         $database -> real_escape_string($this -> firstName),
         $database -> real_escape_string($this -> lastName),
@@ -82,10 +82,10 @@ class User{
         $database -> real_escape_string($this -> weight))); 
 
         if ($result === TRUE) {
-            echo "New record created successfully\n";
+            echo "New user record created successfully<br>";
         }
         else{
-            echo "Error\n";
+            echo "Error<br>";
         }
     }
 
@@ -94,30 +94,32 @@ class User{
             $dependent -> create_entry($database);
         }
     }
-
-    public function registerUser($database){    //public function to register the user
-        $this -> create_login_details_entry($database);
-        $this -> create_user_entry($database);
-
+    private function create_user_medicalConcerns($database){
         if(count($this -> medicalConcerns) != 0){   //has medical concerns
             foreach($this -> medicalConcerns as $i){
                 $result = $database -> query(sprintf("INSERT INTO `user_medical_concern`
                 (`user_id`, 
                 `medical_concern`) 
                 VALUES 
-                (UUID_TO_BIN('%s', 1),'%s'", 
+                (UUID_TO_BIN('%s', 1),'%s')", 
                 $database -> real_escape_string($this -> userID),
                 $database -> real_escape_string($i)));
 
                 if ($result === TRUE) {
-                    echo "New record created successfully\n";
+                    echo "New  medical concern record created successfully<br>";
                 }
                 else{
-                    echo "Error\n";
+                    echo "Error<br>";
                 }
             }
         }
-        $this -> create_user_dependents($database); //finnaly, create the entries for the dependents
+    }
+
+    public function registerUser($database){    //public function to register the user
+        $this -> create_login_details_entry($database);
+        $this -> create_user_entry($database);
+        $this -> create_user_medicalConcerns($database); 
+        $this -> create_user_dependents($database); //finally, create the entries for the dependents
     }
 }
 
