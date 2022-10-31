44ecba6dbc300596a615a4be251a293bb38d0630
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-10-31 17:06:56 +0530

    Create user_dependent.php

---
+++ src/user/user_dependent.php
@@ -0,0 +1,40 @@
+<?php
+class UserDependent{
+    private $ownerID;
+    private $name;
+    private $relationship;
+    private $contactNo;
+
+    public function __construct($name, $relationship, $contactNo){
+        $this -> name = $name;
+        $this -> relationship = $relationship;
+        $this -> contactNo = $contactNo;
+    }
+    public function setOwner($owner){
+        $this -> ownerID = $owner;
+    }
+
+    public function create_entry($database){
+        $result = $database -> query(sprintf("INSERT INTO `user_dependent`
+        (`owner_id`, 
+        `name`, 
+        `relationship`, 
+        `contact_num`) 
+        VALUES 
+        ('UUID_TO_BIN(%s, true)','%s','%s','%s')", 
+        $database -> real_character_string($this->ownerID), 
+        $database -> real_character_string($this->name), 
+        $database -> real_character_string($this->relationship), 
+        $database -> real_character_string($this->contactNo)));
+
+        if ($result === TRUE) {
+            echo "New record created successfully\n";
+        }
+        else{
+            echo "Error\n";
+        }
+    }
+}
+
+
+?>
