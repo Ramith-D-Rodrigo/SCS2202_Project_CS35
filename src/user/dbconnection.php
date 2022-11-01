57d5812d8893e037d8319fe340555712341b6591
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-11-01 13:05:06 +0530

    comment out the echo

--- src/user/dbconnection.php
+++ src/user/dbconnection.php
@@ -12,5 +12,5 @@
     if ($connection -> connect_error){
         die("Connection failed: " . $connection -> connect_error);  //connection failure
     }
-    echo "Connected successfully\n";
+    //echo "Connected successfully\n";
 ?>
