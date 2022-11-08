7475cdfdd26858780f181e399f655a7a2999a11c
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-11-08 18:54:17 +0530

    add icons to header

--- public/general/header.php
+++ public/general/header.php
@@ -37,13 +37,19 @@
         <div>
         <?php
             if(isset($_SESSION['userid']) && isset($_SESSION['userrole'])){ //user logged in
-        ?>  <button class ="btn" onclick="window.location.href='/controller/general/logout.php'" style="float:right">LOG OUT</button>
+        ?>  
+            <div style="float:right">
+                <button class ="btn" onclick="window.location.href='/controller/general/logout.php'"><img src="/styles/icons/profile_icon.svg" class="acc-img"></button>
+                <button class ="btn" onclick="window.location.href='/controller/general/logout.php'">Log Out<img src="/styles/icons/logout_icon.svg" class="acc-img"></button>
+            </div>
         <?php
             }
             else{
         ?>
-            <button class="btn" onclick="window.location.href='/public/general/register.php'">REGISTER</button>
-            <button class="btn" onclick="window.location.href='/public/general/login.php'">LOG IN</button>
+            <div style="float:right">
+                <button class="btn" onclick="window.location.href='/public/general/register.php'">Register<img src="/styles/icons/register_icon.svg" class="acc-img"></button>
+                <button class="btn" onclick="window.location.href='/public/general/login.php'">Log in<img src="/styles/icons/login_icon.svg" class="acc-img"></button>
+            </div>
         <?php
             }
         ?>
