6b28dd2e2ef2200064f840ebbd4ed94e5ba7f1ae
Dhack99 <2019cs063@stu.ucsc.cmb.ac.lk>
2022-11-11 15:16:33 +0530

    init

---
+++ public/coach/coach_dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../styles/coach/styles.css">
+        <link rel="stylesheet" href="../../styles/coach/coach_dashboard.css">
+
+        <title>Dashboard</title>
+    </head>
+    <body>
+        <?php
+                require_once("header.php");
+            ?>
+          
+            <br>
+            <div class="main-body">
+                <div class="today-Session"> </div>
+                <div class="payment-income"> </div>
+                <div class="feedback"> </div>
+
+            </div>
+            <?php
+                require_once("footer.php");
+            ?>
+    </body>
+</html>
---
+++ public/coach/header.php
@@ -0,0 +1,67 @@
+<header>
+    <div class='header-top'>
+        <div> <!-- For the current visiting page of the site -->
+            <?php
+                if($_SERVER['REQUEST_URI'] === '/index.php'){    //home page
+                    echo "Welcome";
+                }
+                else if($_SERVER['REQUEST_URI'] === '/public/general/login.php' || $_SERVER['REQUEST_URI'] === '/public/user/user_login.php'){    //login
+                    echo "Log In";
+                }
+                else if($_SERVER['REQUEST_URI'] === '/public/general/register.php'){   //register
+                    echo "Register";
+                }
+                else if($_SERVER['REQUEST_URI'] === '/public/user/user_register.php'){ //user register
+                    echo "User Registration";
+                }
+                else if($_SERVER['REQUEST_URI'] === '/public/general/our_branches.php'){ //user register
+                    echo "Our Branches";
+                }
+                else if($_SERVER['REQUEST_URI'] === 'public/general/our_feedback.php'){ //user register
+                    echo "Our Feedback";
+                }
+                else if($_SERVER['REQUEST_URI'] === '/public/general/reg_caoches.php'){ //user register
+                    echo "Registered Coaches";
+                }
+                else if($_SERVER['REQUEST_URI'] === '/public/general/about_us.php'){ //user register
+                    echo "About Us";
+                }
+                else if($_SERVER['REQUEST_URI'] === '/public/general/search_results.php'){  //search sport
+                    echo "Search a Sport";
+                }
+            ?>
+        </div>
+        <div> <!--- sports complex name -->
+           Example Sports Complex
+        </div>
+        <div>
+        <?php
+            if(isset($_SESSION['userid']) && isset($_SESSION['userrole'])){ //user logged in
+        ?>  
+            <div style="float:right" id="logout">
+                <button class ="btn" onclick="window.location.href='/controller/general/logout.php'"><img src="/styles/icons/profile_icon.svg" class="acc-img"></button>
+                <button class ="btn" onclick="window.location.href='/controller/general/logout.php'">Log Out<img src="/styles/icons/logout_icon.svg" class="acc-img"></button>
+            </div>
+        <?php
+            }
+            else{
+        ?>
+            <div style="float:right" id="login">
+                <button class="btn" onclick="window.location.href='/public/general/register.php'">Register<img src="/styles/icons/register_icon.svg" class="acc-img"></button>
+                <button class="btn" onclick="window.location.href='/public/general/login.php'">Log in<img src="/styles/icons/login_icon.svg" class="acc-img"></button>
+            </div>
+        <?php
+            }
+        ?>
+        </div>
+    </div>
+
+    <nav class="header-links">
+        <a href="/index.php">Home</a>
+        <a href="/public/general/our_sports.php">Our Sports</a>
+        <a href="/public/general/our_branches.php">Our Branches</a>
+        <a href="/public/general/reg_coaches.php">Registered Coaches</a>
+        <a href="/public/general/our_feedback.php">Our Feedback</a>
+        <a href="/public/general/about_us.php">About Us</a>
+    </nav>
+</header>
