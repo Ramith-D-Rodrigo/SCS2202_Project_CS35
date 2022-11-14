529238ed2868cd19d692bd0ea0182e3e07f690b2
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-11-14 15:27:05 +0530

    finish our sports page

--- public/general/our_sports.php
+++ public/general/our_sports.php
@@ -11,36 +11,46 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/styles/general/styles.css">
+        <link rel="stylesheet" href="/styles/general/content_icons.css">
         <title>Our Sports</title>
     </head>
     <body>
         <?php
             require_once("header.php");
         ?>
-        <main>
-            <div class="content-box">
+        <main style="display:flex; justify-content:center; align-items:center;">
+            <div class="content-box sports-content-box">
                 <?php
                     if(isset($_SESSION['our_Sports'])){
                         foreach($_SESSION['our_Sports'] as $sport){
                 ?>
-                        <form class="search_result" method="post" action="/controller/general/user_selection_controller.php">
-                            Sport : <?php echo $sport['sport_name'] ?>
-                            <br>
-                            Reservation Price : Rs .<?php echo $sport['reserve_price'] ?> per hour
-                            <br>
-                            Available Branches : 
-                            <select required name="selected_branch">
-                                <option value=""></option>
-                                <?php foreach($sport['providing_branches'] as $branches){
-                                ?>
-                                    <option value="<?php echo $branches['branch_id']?>"><?php echo $branches['branch_name']?></option>
-                            <?php
-                                }
-                            ?>
-                            </select>
-                            <br>
-                            <button type="submit" name="selectedSportBtn" value="<?php echo $sport['sport_id']?>">Make a Reservation</button>
-                        </form>
+                        <div class="sports-row">
+                            <div class="sport-icon-container">
+                                <img class ="sport-icon" src="/styles/icons/sports/<?php echo strtolower($sport['sport_name']).'.jpg'?>" onerror="this.src='/styles/icons/no-results.png'"/>
+                            </div>
+                            
+                            <div>
+                                <form class="search_result" method="post" action="/controller/general/user_selection_controller.php">
+                                    Sport : <?php echo $sport['sport_name'] ?>
+                                    <br>
+                                    Reservation Price : Rs .<?php echo $sport['reserve_price'] ?> per hour
+                                    <br>
+                                    Available Branches : 
+                                    <select required name="selected_branch">
+                                        <option value=""></option>
+                                        <?php foreach($sport['providing_branches'] as $branches){
+                                        ?>
+                                            <option value="<?php echo $branches['branch_id']?>"><?php echo $branches['branch_name']?></option>
+                                    <?php
+                                        }
+                                    ?>
+                                    </select>
+                                    <br>
+                                    <button type="submit" name="selectedSportBtn" value="<?php echo $sport['sport_id']?>">Make a Reservation</button>
+                                </form>
+                            </div>
+                        </div>
+
                 <?php
                         }   
 
