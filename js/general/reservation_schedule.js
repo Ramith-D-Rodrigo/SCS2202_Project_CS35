e28af64272532cacb4b55b6a2f92ed5a2f01650f
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-11-18 15:34:35 +0530

    fixed some errors, still has more to fix

--- js/general/reservation_schedule.js
+++ js/general/reservation_schedule.js
@@ -51,9 +51,10 @@ for(i = 0; i < schedules.length; i++){
         const endingTime = reservationInfo.item(j).children.item(2).innerHTML.split(":"); //2nd index is ending time
 
         //create date objects for the reservation starting and ending times
-        
-        const startingTimeDateObj = new Date(resDate[0], resDate[1], resDate[2], startingTime[0], startingTime[1], startingTime[2]);
-        const endingTimeDateObj = new Date(resDate[0], resDate[1], resDate[2], endingTime[0], endingTime[1], endingTime[2]);
+
+        //month - 1 because JS month starts from 0
+        const startingTimeDateObj = new Date(resDate[0], resDate[1] - 1, resDate[2], startingTime[0], startingTime[1], startingTime[2]);
+        const endingTimeDateObj = new Date(resDate[0], resDate[1] - 1, resDate[2], endingTime[0], endingTime[1], endingTime[2]);
 
         //console.log(startingTimeDateObj, endingTimeDateObj);
 
@@ -89,20 +90,30 @@ for(i = 0; i < schedulesArr.length; i++){
         tableRow.appendChild(currDay);
     } */
     let reservationCells = [];
+    let datesWithReservations = []; //array to store the dates that has reservations
 
     for(j = 0; j < schedulesArr[i].length; j++){
         const res = schedulesArr[i][j];
         const resDate = res.startingTime.toISOString().split("T")[0];
-        const resHead = tableRow.insertCell();
-        resHead.innerText = resDate;
+
+        if(!Object.keys(datesWithReservations).includes(resDate)){ //current Reservation is not in the datesWithReservation (first reservation that is found for that date)
+            const resHead = tableRow.insertCell();
+            resHead.innerText = resDate;
+            datesWithReservations[resDate] = [res];   //add the date and the corresponding reservation to the array
+        }
+        else{
+            datesWithReservations[resDate].push(res);   //add the reservation of same date to the array
+        }
+
         const cell = document.createElement("td");
         cell.rowSpan = res.timeDiff;
         cell.innerText = "Reserved";
         cell.style.backgroundColor = "Red";
         cell.style.color = "white";
         cell.style.borderRadius = "10px";
-        reservationCells.push(cell);
+        reservationCells[i + "," + resDate + "," + res.startingTime.toLocaleTimeString()] = cell;  //key is => "Court , Reservation Date, Starting Time"
     }
+    //console.log(datesWithReservations);
 
     const openingTime = new Date();
     openingTime.setHours(branchOpeningTime[0]);
@@ -120,6 +131,7 @@ for(i = 0; i < schedulesArr.length; i++){
 
     while(currTime < closingTime){
         const tableRow = table.insertRow();
+
         const timePeriod = tableRow.insertCell();
 
         const periodStartPrint = currTime.toLocaleTimeString(); //print to print in the page
@@ -130,29 +142,58 @@ for(i = 0; i < schedulesArr.length; i++){
         const periodEndPrint = currTime.toLocaleTimeString();
         const periodEndCompare = currTime.toTimeString();
 
+        tableRow.id = i + "," + periodStartPrint + "," + periodEndPrint;
+
         timePeriod.innerText = periodStartPrint + " - " + periodEndPrint;
 
-        for(j = 0; j < schedulesArr[i].length; j++){    //adding the reservation
-            const currRes = schedulesArr[i][j];
-            const currResDate = currRes.startingTime.toISOString().split("T")[0];
+        for(let key in datesWithReservations){    //adding the reservation to the table
+/*             const resOnThatDate = datesWithReservations[key];
+            //console.log(currRes);
+            //const currResDate = currRes.startingTime.toISOString().split("T")[0];
 
             //get the time in hours format
-            const currResST = currRes.startingTime.toTimeString();
-            const currResET = currRes.endingTime.toTimeString();
+            const reservations = resOnThatDate.map(i => [i.startingTime.toTimeString(), i.endingTime.toTimeString()]); //map it to pairs
 
-            if(periodStartCompare === currResST && table.rows[0].cells[j+1].innerText === currResDate){  //found the reservation starting point
-                tableRow.appendChild(reservationCells[j]);
+            //console.log(currResST, currResET);
+
+            if(reservations.map(i => i[0]).includes(periodStartCompare)){  //found the reservation starting point
+                //console.log(key + " " + periodStartPrint);
+                tableRow.appendChild(reservationCells[key + " " + periodStartPrint]);
             }
-            else if(table.rows[0].cells[j+1].innerText === currResDate && periodStartCompare >= currResST && periodEndCompare <= currResET){    //within a reservation block
+            else if(true){    //within a reservation block
                 continue;   //do not enter any cell
             }
             else{
                 const cell = tableRow.insertCell();
-            }
+            } */
             //console.log(cell.cellIndex);
+            const cell = tableRow.insertCell();
+
+            cell.id = i + "," + key  + "," + periodStartPrint;  //unique cell id
+            //console.log(cell.id.length + " " + cell.id);
         }
 
     }
 
     schedules.item(i).appendChild(table);
+
+    for(cell in reservationCells){
+        const insertingCell = document.getElementById(cell);
+        const row = insertingCell.parentNode;
+        reservationCells[cell].id = insertingCell.id;
+        const cellID = insertingCell.cellIndex;
+        row.replaceChild(reservationCells[cell], insertingCell);
+
+        const rowSpan = reservationCells[cell].rowSpan;
+        var i = 1;
+        var curRow = row;
+
+        while(i < rowSpan){
+            const sibling = curRow.nextSibling;
+            console.log(sibling.childNodes.item(cellID));
+            sibling.removeChild(sibling.childNodes.item(cellID));
+            curRow = sibling;
+            i++;
+        }
+    }
 }
