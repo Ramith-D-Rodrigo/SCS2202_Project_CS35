bb93879ecc5166eea2396fc47e2428faca35263c
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2022-12-20 16:34:41 +0530

    Create actor.php

---
+++ src/general/actor.php
@@ -0,0 +1,16 @@
+<?php
+    class Actor{
+        private $emailAddress;
+        private $username;
+        private $userID;
+        private $userRole;
+        private $password;
+        private $connection;
+
+        public function login($inputUsername, $inputPassword){
+            
+        }
+    }
+
+
+?>
