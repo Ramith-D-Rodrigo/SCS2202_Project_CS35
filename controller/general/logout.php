30c9120b0d47c1a7338db04c3dc4c823bee8eac1
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-12-21 23:18:20 +0530

    add session unset to logout

--- controller/general/logout.php
+++ controller/general/logout.php
@@ -4,10 +4,8 @@
     //echo "log out button";
 
     //print_r($_SESSION);
-
-    $result = session_destroy();  //clear the session
-
-
+    session_unset();    //clear the session
+    session_destroy();  //clear the session
     //echo $result;   //print session destruction result
 
     //print_r($_SESSION);
