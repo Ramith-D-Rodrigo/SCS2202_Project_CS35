4dfe7641c4c9f8bb9b6ec1cf5054e0d9da110fb5
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-12-29 03:50:58 +0530

    activateAccount function for mailer

--- src/general/mailer.php
+++ src/general/mailer.php
@@ -39,9 +39,19 @@
         public static function registerAccount($recipientEmail, $recipient_name, $mailVerificationCode){   //this function is used to send the verification code to the user's email upon registration
             $subject = "Account Registration";
             $body = "<p>Hi $recipient_name,</p>
-                    <p>Thank you for registering with us. Your account is not registered yet. Please use the following verification code to activate your account.</p>
+                    <p>Thank you for registering with us. Your account is not activated yet. Please use the following verification code to activate your account.</p>
                     <p>Code : {$mailVerificationCode} </p>
                     <p>If you did not register with us, please ignore this email.</p>
+                    <p>Regards,<br>Sportude Support</p>";
+            return self::sendMail($recipientEmail, $recipient_name, $subject, $body);
+        }
+
+        public static function activateAccount($recipientEmail, $recipient_name, $mailVerificationCode){   //this function is used to send the verification code for account activation
+            $subject = "Account Activation";
+            $body = "<p>Hi $recipient_name,</p>
+                    <p>Your account is not activated. Please use the following verification code to activate your account.</p>
+                    <p?>Code : {$mailVerificationCode} </p>
+                    <p>If you did not request to activate your account, please ignore this email.</p>
                     <p>Regards,<br>Sportude Support</p>";
             return self::sendMail($recipientEmail, $recipient_name, $subject, $body);
         }
