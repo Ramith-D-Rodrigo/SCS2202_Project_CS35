2ea4ae3a02bad48aed438f83511fc287cb844648
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2023-01-20 23:49:37 +0530

    email, username validation functions

--- src/general/security.php
+++ src/general/security.php
@@ -28,6 +28,49 @@
 
             return array($email, $username, $userRole, $userID);
         }
+
+        public static function checkEmailAvailability($email){   //checks if the email address is available
+            $santizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL); //sanitize the email address
+
+            if(!filter_var($santizedEmail, FILTER_VALIDATE_EMAIL)){ //check if the email is valid
+                return false;
+            }
+
+            require("dbconnection.php");
+            self::$connection = $connection;
+            $sql = sprintf("SELECT `email_address` FROM `login_details` WHERE email_address = '%s'", self::$connection -> real_escape_string($santizedEmail));
+
+            $result = self::$connection -> query($sql);
+            $row = $result -> fetch_object();
+            self::$connection -> close();
+
+            if($row === null){  //if the email address is not in the database, it is available
+                return true;
+            }
+            else{
+                $row -> free_result();
+                return false;
+            }
+        }
+
+        public static function checkUsernameAvailability($username){
+            require("dbconnection.php");
+            self::$connection = $connection;
+            $sql = sprintf("SELECT `username` FROM `login_details` WHERE username = '%s'", self::$connection -> real_escape_string($username));
+
+            $result = self::$connection -> query($sql);
+
+            $row = $result -> fetch_object();
+            self::$connection -> close();
+
+            if($row === null){  //if the username is not in the database, it is available
+                return true;
+            }
+            else{
+                $row -> free_result();
+                return false;
+            }
+        }
     }
 
 ?>
