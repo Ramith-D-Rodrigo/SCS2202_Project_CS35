a4fd0255ae8f7058e1d93bf8ce3e3d9b0a65f017
dihandesilva22 <dihandesilva2022@gmail.com>
2023-02-04 13:20:52 +0530

    fix stylings of pages

--- public/receptionist/coach_profile.php
+++ public/receptionist/coach_profile.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/general/styles.css">
     <link rel="stylesheet" href="/styles/general/staff.css">
+    <link rel="stylesheet" href="/styles/receptionist/receptionist.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <title>Coach Profile</title>
 </head>
@@ -20,7 +21,7 @@
     <?php
         require_once("dashboard_header.php");
     ?>
-    <main>
+    <main class="body-container">
     <div style="display:flex;flex-direction:row" id="search_results">
         <div class="content-box" style="width:45%" id="profileData">
             <div id="profilePic" style="display:flex; justify-content:center;margin-bottom:5%">
@@ -91,6 +92,7 @@
             <div id="errFeedback-msg">
             </div>   
             <div id="feedback"> </div>
+        </div>
     </div>
     </main>
     <?php
--- public/receptionist/view_reservations.php
+++ public/receptionist/view_reservations.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/general/styles.css">
     <link rel="stylesheet" href="/styles/general/staff.css">
-    <link rel="stylesheet" href="/styles/general/staff.css">
+    <link rel="stylesheet" href="/styles/receptionist/viewReservations.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <title>View Reservations</title>
 </head>
