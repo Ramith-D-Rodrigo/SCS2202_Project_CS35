2c5397004df36b6d605e4a2eae6ab532e5ccbb21
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-02-07 11:17:21 +0530

    coaching sessions page

--- public/general/header.php
+++ public/general/header.php
@@ -87,8 +87,7 @@
         <a href="/public/general/our_feedback.php">Our Feedback</a>
         <a href="/public/general/about_us.php">About Us</a>
         <div id="profile-links" style="float:right; margin-right:20px; display:none">
-            <a href="/index.php">Ongoing Sessions</a>
-            <a href="/index.php">Left Sessions</a>
+            <a href="/public/user/coaching_sessions.php">Coaching Sessions</a>
             <a href="/public/user/reservation_history.php">Reservation History</a>
             <a href="/public/user/edit_profile.php">Edit Profile</a>
         </div>
