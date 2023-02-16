28db5b9fc086d8c39e67b1128585bc88f008a585
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2023-02-16 21:47:43 +0530

    handle errors

--- src/general/paymentGateway.php
+++ src/general/paymentGateway.php
@@ -5,24 +5,22 @@
 
     class paymentGateway{
         // Set your Stripe API keys
-        public static function chargePayment($stripeToken, $chargeAmount, $chargeDescription, $currency){
-
-            
+        public static function chargePayment($stripeToken, $chargeAmount, $chargeDescription, $currency): array{    //returns the array with the status and the message
             \Stripe\Stripe::setApiKey(STRIPE_API_KEY);
             try {
                 // Create a charge
                 $charge = \Stripe\Charge::create(array(
-                    'amount' => $chargeAmount,
+                    'amount' => $chargeAmount * 100,    //to convert the amount to cents
                     'currency' => $currency,
                     'description' => $chargeDescription,
                     'source' => $stripeToken
                 ));
 
                 if($charge -> paid == true){    //if the payment is successful
-                    return true;
+                    return [true, "Payment Successful"];
                 }
                 else{
-                    return false;
+                    return [false, "Payment Failed"];
                 }
             /*} catch(\Stripe\Exception\CardException $e) {   // Handle card errors
                 echo "CardException: " . $e -> getMessage();
@@ -48,14 +46,37 @@
                 echo "ApiErrorException: " . $e -> getMessage();
                 return false;
             */
-            } catch (Exception $e) {     // Handle other errors
+            } catch (\Stripe\Exception\CardException $e) {     // Handle other errors
                 //echo "Exception: " . $e -> getMessage();
-                return false;
+                $errCode = $e -> getError() -> code;
+                switch($errCode){
+                    case 'card_declined':
+                        return [false, "Card Declined"];
+                        break;
+                    case 'incorrect_cvc':
+                        return [false, "Incorrect CVC"];
+                        break;
+                    case 'expired_card':
+                        return [false, "Expired Card"];
+                        break;
+                    case 'incorrect_number':
+                        return [false, "Incorrect Card Number"];
+                        break;
+                    case 'incorrect_zip':
+                        return [false, "Incorrect Zip Code"];
+                        break;
+                    case 'processing_error':
+                        return [false, "Processing Error"];
+                        break;
+                    default:
+                        return [false, "Payment Failed"];
+                        break;
+                }
             }
         }
 
-        public static function userReservationPayment($reservationPrice, $reservationDescription, $currency, $stripeToken){
-            return self::chargePayment($stripeToken, $reservationPrice, $reservationDescription, $currency);
+        public static function userReservationPayment($reservationPrice, $description, $currency, $stripeToken){
+            return self::chargePayment($stripeToken, $reservationPrice, $description, $currency);
         }
     }
 
