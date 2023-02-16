1f3f95c1033df0dcbf57bcec7b61efbeb473d54d
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2023-02-16 21:46:58 +0530

    complete frontend of make reservation payment gateway

--- js/user/make_reservation.js
+++ js/user/make_reservation.js
@@ -4,12 +4,52 @@ import {updateTheReservationTables, createScheduleObjects} from '../general/rese
 
 import { currency } from '../CONSTANTS.js';
 
+let sendingRequest = null;  //to store the reservation details
+let stripe = null;  //to store the payment gateway
+let cardElement = null;  //to store the card element
+
+const errorElement = document.getElementById("card-errors");
+const paymentGatewayPopup = document.getElementById("paymentBox");
+const main = document.querySelector("main");
+
+
+const animateGateWayClosing = () => {
+    paymentGatewayPopup.animate([
+        {top: "50%", transform: "translate(-50%, -50%)", opacity: 1},
+        {top: "-50%", transform: "translate(-50%, -50%)", opacity: 0}
+    ], {
+        duration: 500,
+        easing: "ease-in-out"
+    })
+    .onfinish = () => {
+        paymentGatewayPopup.style.display = "none";
+        main.style.opacity = "1";
+        //clear the card element
+        cardElement.clear();
+        errorElement.textContent = "";
+    };
+};
+
+//get the publishable key from the server
+fetch("../../controller/general/get_stripe_publishable_key_controller.php")
+    .then((res) => res.json())
+    .then((data) => {   //set the payment gateway
+        //console.log(data);
+        stripe = Stripe(data.publishableKey);
+
+        const elements = stripe.elements();
+
+        cardElement = elements.create("card");    //create the card element
+        cardElement.mount("#card-element");
+    });
+
+
 reservationForm.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent the form from submitting
     const formData = new FormData(reservationForm);
     const reserveBtn = document.getElementById("makeReserveBtn");
 
-    const sendingRequest = {
+    sendingRequest = {
         "numOfPeople" : formData.get("numOfPeople"),
         "reservingStartTime" : formData.get("reservingStartTime"),
         "reservingEndTime" : formData.get("reservingEndTime"),
@@ -17,31 +57,56 @@ reservationForm.addEventListener('submit', (event) => {
         "makeReserveBtn" : reserveBtn.value
     };
 
-    let stripe = null;
+    //add the amount to the payment gateway
+    const amount = document.getElementById("amount");
+    amount.innerHTML = "You are paying " + currency + " ";
+    amount.innerHTML += formData.get("reservationPrice");
 
-    //get the publishable key from the server
-    fetch("../../controller/general/get_stripe_publishable_key_controller.php")
-    .then((res) => res.json())
-    .then((data) => {
-        //console.log(data);
-        stripe = Stripe(data.publishableKey);
+    //payment gateway popup div display
+    paymentGatewayPopup.style.display = "block";
 
-        const elements = stripe.elements();
+    //scroll to the payment gateway and animate it
+    paymentGatewayPopup.scrollIntoView({behavior: "smooth", block: "center"});
+    
+    //lower the opacity of the main content
+    main.style.opacity = "0.5";
 
-        const cardElement = elements.create("card");    //create the card element
-        cardElement.mount("#card-element");
+    //animate the opacity
+    main.style.transition = "opacity 0.5s ease-in-out";
 
-        //add the amount
-        const amount = document.getElementById("amount");
-        amount.innerHTML += currency + " ";
-        amount.innerHTML += formData.get("reservationPrice");
+    //click event listener for the close button
+    main.addEventListener("click", function mainClick(){
+        animateGateWayClosing();
+        //remove the event listener
+        main.removeEventListener("click", mainClick);
+    });
+});
 
-    })
+//payent form event listener
+const paymentForm = document.querySelector("#payment-form");
 
-    return;
+paymentForm.addEventListener("submit", (event) => {
+    event.preventDefault(); //prevent the form from submitting
+    stripe.createToken(cardElement).then((result) => {  //create the token
+        if(result.error){   //if there is an error
+            errorElement.textContent = result.error.message;
+        }
+        else{
+            //animate the payment gateway popup
+            animateGateWayClosing();
+            main.click();
 
+            //send the token to the server
+            stripeTokenHandler(result.token);
+        }
+    });
+});
 
-    //send the reservation details to the server
+//send the token to the server
+const stripeTokenHandler = (token) => {
+    const tokenID = token.id;
+    sendingRequest.tokenID = tokenID;   //add the token to the request
+
     fetch("../../controller/user/make_reservation_controller.php", {
         method: "POST",
         headers: {
@@ -51,6 +116,7 @@ reservationForm.addEventListener('submit', (event) => {
     })
     .then((res) => res.json())
     .then((data) => {
+        //console.log(data);
         if(data.successMsg !== undefined){  //reservation success
             const successMsgBox = document.getElementById("successMsg");
             const errMsgBox = document.getElementById("errMsg");
@@ -65,6 +131,7 @@ reservationForm.addEventListener('submit', (event) => {
             const getReq = params.get("reserveBtn");
             //console.log(getReq);
 
+            //update the reservation table
             fetch("../../controller/general/reservation_schedule_controller.php?reserveBtn=".concat(getReq))
                 .then(res => res.json())
                 .then(data => {
@@ -84,6 +151,5 @@ reservationForm.addEventListener('submit', (event) => {
     })
     .catch((err) => {
         console.log(err);
-    }
-    );
-});
+    });
+}
