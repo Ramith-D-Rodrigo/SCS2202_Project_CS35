f36da60bd9e9a6f1a334ada677932142175a654c
dihandesilva22 <dihandesilva2022@gmail.com>
2023-02-21 09:58:14 +0530

    modify the errMsg validation

--- js/system_admin/staff_register_validation.js
+++ js/system_admin/staff_register_validation.js
@@ -3,12 +3,6 @@ function validateForm(event){
     const password = document.getElementById("password");
     const cPassword = document.getElementById("cPassword");
 
-    if (errMsg.innerHTML.trim() !== "") {   //trim method to remove any leading or trailing whitespaces
-        event.preventDefault(); //do not submit
-        return false;
-    } else {
-        errMsg.innerHTML = '' //empty before the validation
-    }
     const form = document.querySelector("form");
 
     if(verbose){
@@ -16,7 +10,7 @@ function validateForm(event){
     }
 
     if(password.value != cPassword.value) {    //compare the password and the confirm password fields
-        errMsg.innerHTML = errMsg.innerHTML + "Passwords are mismatched";
+        errMsg.innerHTML = errMsg.innerHTML + "Passwords are Mismatched";
         return false;
     }
 
@@ -24,5 +18,8 @@ function validateForm(event){
         errMsg.innerHTML = errMsg.innerHTML + "Please Add Valid Information";
         return false;
     }
+
+    errMsg.innerHTML = '';
+    return true;
     
 }
