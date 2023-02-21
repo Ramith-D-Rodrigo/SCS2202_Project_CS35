6d19d9aa28d4753470b9007c98f995f0f672ca13
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2023-02-21 08:48:08 +0530

    deactivate account frontend complete

--- js/user/deactivate_account.js
+++ js/user/deactivate_account.js
@@ -21,15 +21,19 @@ function validateDeactivateForm(event){
     }
 }
 
-deactivateForm.addEventListener("submit", (e) => {
+deactivateForm.addEventListener("submit", function deactivatePasswordCheck(e){
     e.preventDefault();
     const formData = new FormData(deactivateForm);
     const sendingReq = Object.fromEntries(formData);
 
     let msgDiv = null;
+    let success = null; //check the response status
     
     //add the please wait message
     deactivateSuccessMsgBox.innerHTML = "Please Wait...";
+
+    //disable the deactivate button
+    deactivateBtn.disabled = true;
       
     //send the data to the server
     fetch("../../controller/user/deactivate_account_validation_controller.php", {
@@ -41,10 +45,26 @@ deactivateForm.addEventListener("submit", (e) => {
     })
     .then(res =>{
         deactivateSuccessMsgBox.innerHTML = "";
-        if(res.ok){
-            msgDiv = deactivateSuccessMsgBox;
-            deactivateForm.reset();
+        deactivateErrMsgBox.innerHTML = "";
 
+        //enable the deactivate button
+        deactivateBtn.disabled = false;
+
+        if(res.ok){ //if the response status is ok
+            msgDiv = deactivateSuccessMsgBox;
+            success = true;
+            deactivateForm.reset();         
+        }
+        else{   //if the response status is not ok
+            msgDiv = deactivateErrMsgBox;
+            success = false;
+        }
+        return res.json();
+    })
+    .then(data => { //extract the json data from the response
+        msgDiv.innerHTML = data.msg;
+
+        if(success){    //if the response status is ok
             //add the verification code input field
             const verificationCodeDiv = document.createElement("div");
             verificationCodeDiv.className = "left-field";
@@ -58,7 +78,7 @@ deactivateForm.addEventListener("submit", (e) => {
             verificationCodeInput.required = true;
             verificationCodeInput.type = "text";
             verificationCodeInput.name = "verificationCode";
-            
+
             rowContainer.appendChild(verificationCodeDiv);
             rowContainer.appendChild(verificationCodeInput);
 
@@ -71,14 +91,69 @@ deactivateForm.addEventListener("submit", (e) => {
             //remove the onclick attribute of the deactivate button
             deactivateBtn.removeAttribute("onclick");
             deactivateBtn.innerHTML = "Verify the Code";
-            
+
+            //remove the submit event listener of the form
+            deactivateForm.removeEventListener("submit", deactivatePasswordCheck);
+
+            //add the submit event listener to the form
+            deactivateForm.addEventListener("submit", (e) => {
+                e.preventDefault();
+
+                deactivateErrMsgBox.innerHTML = "";
+                deactivateSuccessMsgBox.innerHTML = "";
+                
+                //simple verification code validation
+                if(verificationCodeInput.value.length !== 6){
+                    deactivateErrMsgBox.innerHTML = "Invalid verification code";
+                    return;
+                }
+
+                //disable the button
+                deactivateBtn.disabled = true;
+
+
+                const verificationCode = verificationCodeInput.value;
+
+                const lastReq = {
+                    verificationCode: verificationCode
+                };
+
+                deactivateSuccessMsgBox.innerHTML = "You will be logged out after the deactivation process.<br> Please Wait...";
+
+                setTimeout(() => {
+                    fetch("../../controller/user/deactivate_account_controller.php", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type" : "application/json"
+                        },
+                        body: JSON.stringify(lastReq)
+                    })
+                    .then(res => {
+
+                        deactivateSuccessMsgBox.innerHTML = "";
+                        deactivateErrMsgBox.innerHTML = "";
+
+                        if(res.ok){ //successful deactivation
+                            window.location.href = "../../index.php";
+                            success = true;
+                        }
+                        else{   //unsuccessful deactivation
+                            msgDiv = deactivateErrMsgBox;
+                            success = false;
+
+                            //re-enable the button
+                            deactivateBtn.disabled = false;
+
+                        }
+                        return res.json();
+                    })
+                    .then((data) => {
+                        if(!success){   //deactivation unsuccessful
+                            msgDiv.innerHTML = data.msg;    //show the error message
+                        }
+                    })
+                }, 1000);
+            });
         }
-        else{
-            msgDiv = deactivateErrMsgBox;
-        }
-        return res.json();
-    })
-    .then(data => {
-        msgDiv.innerHTML = data.msg;
     })
 });
