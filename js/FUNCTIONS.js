c3446b00b98883185c93f74d66f51396266aace6
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2023-02-26 00:34:02 +0530

    add two more functions

--- js/FUNCTIONS.js
+++ js/FUNCTIONS.js
@@ -18,4 +18,36 @@ const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export {changeToLocalTime, capitalizeFirstLetter};
+const disableElementsInMain = (mainElement) => {
+    const buttons = mainElement.querySelectorAll("button");
+    const inputs = mainElement.querySelectorAll("input");
+    const selects = mainElement.querySelectorAll("select");
+    const textareas = mainElement.querySelectorAll("textarea");
+    const radios = mainElement.querySelectorAll("radio");
+    const checkboxes = mainElement.querySelectorAll("checkbox");
+
+    const elements = [...buttons, ...inputs, ...selects, ...textareas, ...radios, ...checkboxes];
+
+    elements.forEach((element) => {
+        element.disabled = true;
+    });
+
+}
+
+const enableElementsInMain = (mainElement) => {
+    const buttons = mainElement.querySelectorAll("button");
+    const inputs = mainElement.querySelectorAll("input");
+    const selects = mainElement.querySelectorAll("select");
+    const textareas = mainElement.querySelectorAll("textarea");
+    const radios = mainElement.querySelectorAll("radio");
+    const checkboxes = mainElement.querySelectorAll("checkbox");
+
+    const elements = [...buttons, ...inputs, ...selects, ...textareas, ...radios, ...checkboxes];
+
+    elements.forEach((element) => {
+        element.disabled = false;
+    });
+
+}
+
+export {changeToLocalTime, capitalizeFirstLetter, disableElementsInMain, enableElementsInMain};
