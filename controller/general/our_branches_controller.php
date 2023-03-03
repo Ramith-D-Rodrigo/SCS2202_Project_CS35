a8a72bc1e2884774aef833ed1194f71897e818ee
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2023-03-03 08:37:50 +0530

    fix errors in our branches controller

--- controller/general/our_branches_controller.php
+++ controller/general/our_branches_controller.php
@@ -4,6 +4,7 @@
     require_once("../../src/general/website_functions/our_branches_functions.php");
     require_once("../../src/general/branch.php");
     require_once("../../src/manager/manager.php");
+    require_once("../../src/receptionist/receptionist.php");
     
     $allBranches = getAllBranches($connection); //get all branch IDs (in an array)
 
@@ -13,35 +14,34 @@
 
         $tempBranch = new Branch($branchID);    //create new branch object
 
-        $tempBranch -> getDetails($connection);
-        $branchSports = $tempBranch -> getAllSports($connection);
+        $tempBranch -> getDetails($connection, ['address', 'city', 'branchEmail', 'openingTime', 'closingTime', 'currManager', 'currReceptionist']);
+        $branchSports = $tempBranch -> offeringSports($connection);
+
+        //traverse the sports array and get the sport name
+        foreach($branchSports as $currSport){
+            $currSport -> getDetails($connection, ['sportName']);
+        }
         $branchRating = $tempBranch -> getBranchRating($connection);
 
         $branchJSON = json_encode($tempBranch); //encode and decode to unset un-necessary info
         $branchASSOC = json_decode($branchJSON, true);
+
+
+        $branchManager = new Manager(); //to get manager details
+        $branchManager -> setUserID($branchASSOC['currManager']);
+
+        $branchManager -> getDetails(['firstName', 'lastName', 'contactNum', 'gender']);
+
+        $branchReceptionist = new Receptionist(); //to get receptionist details
+        $branchReceptionist -> setUserID($branchASSOC['currReceptionist']);
+
+        $branchReceptionist -> getDetails(['firstName', 'lastName', 'contactNum', 'gender']);
+
         $branchASSOC['sports'] = $branchSports;
         $branchASSOC['rating'] = $branchRating;
-        //manager un-necessary info
-        unset($branchASSOC['manager']['branchID']);
-        unset($branchASSOC['manager']['dateOfBirth']);
-        unset($branchASSOC['manager']['emailAddress']);
-        unset($branchASSOC['manager']['joinDate']);
-        unset($branchASSOC['manager']['leaveDate']);
-        unset($branchASSOC['manager']['managerID']);
-        unset($branchASSOC['manager']['username']);
-        unset($branchASSOC['manager']['staffRole']);
-        unset($branchASSOC['manager']['password']);
+        $branchASSOC['manager'] = $branchManager;
+        $branchASSOC['receptionist'] = $branchReceptionist;
 
-        //receptionist un-necessary info
-        unset($branchASSOC['receptionist']['branchID']);
-        unset($branchASSOC['receptionist']['dateOfBirth']);
-        unset($branchASSOC['receptionist']['emailAddress']);
-        unset($branchASSOC['receptionist']['joinDate']);
-        unset($branchASSOC['receptionist']['leaveDate']);
-        unset($branchASSOC['receptionist']['receptionistID']);
-        unset($branchASSOC['receptionist']['username']);
-        unset($branchASSOC['receptionist']['staffRole']);
-        unset($branchASSOC['receptionist']['password']);
 
         array_push($branchInfo, $branchASSOC);   //push to array;
 
@@ -52,6 +52,6 @@
 
     $connection -> close();
     header('Content-Type: application/json;');    //because we are sending json
-    echo json_encode($branchInfo);
+    echo json_encode($branchInfo, JSON_PRETTY_PRINT);
     unset($branchInfo);
 ?>
