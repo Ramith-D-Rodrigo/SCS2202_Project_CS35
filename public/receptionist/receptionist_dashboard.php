767dd7c22459d83ba0c2e5b7f4ef54eea8834635
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2023-05-06 18:48:26 +0530

    Update receptionist_dashboard.php

--- public/receptionist/receptionist_dashboard.php
+++ public/receptionist/receptionist_dashboard.php
@@ -12,6 +12,7 @@
   <meta charset="UTF-8" />
   <title>Receptionist's Dashboard</title>
   <link rel="stylesheet" href="/styles/general/styles.css">
+  <link rel="stylesheet" href="/styles/general/staff.css">
   <link rel="stylesheet" href="/styles/receptionist/receptionistDashboard.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 
@@ -20,27 +21,28 @@
 <?php
         require_once("dashboard_header.php");
 ?>
-<main >
-    <div class="flex-container">
-            <div  class="tabs" style="min-width:30%; min-height:20rem">
-                <div> Reservations</div> 
+<main>
+    <div class="body-container dashboard-cards">
+            <div  class="content-box single-card">
+                <div class="card-title"> Reservations</div> 
                 <div id="reservations"></div>
-                <div> <button onclick="window.location.href='/public/receptionist/view_reservations.php'"> View More </button> </div>
+                <div class="card-button"> <button onclick="window.location.href='/public/receptionist/view_reservations.php'"> View More </button> </div>
             </div>
-            <div  class="tabs" style="min-width:30%; min-height:20rem">
+            <div  class="content-box single-card">
                 <div> Onsite Reservation</div> 
                 <div id="onlineR"></div>
-                <div> <button style="width:100%" onclick="window.location.href='/public/receptionist/onsite_reservation_entry.php'"> Add </button> </div>
-                <div> <button onclick="window.location.href='/public/receptionist/cancel_onsite_reservations.php'"> Cancel </button> </div>
+                <div class="card-button"> 
+                    <button onclick="window.location.href='/public/receptionist/onsite_reservation_entry.php'"> Add </button> 
+                    <button onclick="window.location.href='/public/receptionist/cancel_onsite_reservations.php'"> Cancel </button>
+                </div>
+                
             </div>
-            <div  class=tabs style="min-width:30%; min-height:20rem">
+            <div class="content-box single-card">
                 <div>Branch Details</div> 
                 <div id="branch"></div>
                 <div> <button onclick="window.location.href='/public/receptionist/edit_branch.php'"> Edit</button> </div>
             </div>   
-        </div>
-        <div class="flex-container">
-            <div  class=tabs style="min-width:30%; min-height:20rem">
+            <div  class="content-box single-card">
                 <div> View Profiles</div> 
                 <div id="profiles"></div>
                 <div> 
@@ -48,12 +50,12 @@
                     <button onclick="window.location.href='/public/receptionist/view_user_profiles.php'"> View User Profiles   </button> 
                 </div>
             </div>
-            <div class=tabs style="min-width:30%; min-height:20rem">
+            <div class="content-box single-card">
                 <div>Request Maintenance</div> 
                 <div id="maintenance"></div>
                 <div> <button onclick="window.location.href='/public/receptionist/request_maintenance.php'" > Add </button> </div>
             </div>  
-        </div>
+
     </div>
 </main>
 <?php
